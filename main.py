a53c1bca5a28b7b0288929a5bc7e502fb20580ab
NicholasHeim <nickheim28@gmail.com>
2021-04-11 10:29:57 -0400

    Remove 2D partition compatibility

--- main.py
+++ main.py
@@ -7,33 +7,9 @@ import csv
 GENERATE_3D_PARTITIONS  = True   # Determine whether or not the program will generate all partitions of size MIN_GENERATION_SIZE <= n <= MAX_GENERATION_SIZE
 MIN_GENERATION_SIZE     = 1      # Expected to be greater than 0
 MAX_GENERATION_SIZE     = 5      # Expected to be greater than the min
-DIMENSIONS              = 3     
-GENERATE_RESULTS     = False  # Requires that GENERATE_3D_PARTITIONS has been run up to and through MAX_GENERATION_SIZE
+GENERATE_RESULTS        = False  # Requires that GENERATE_3D_PARTITIONS has been run up to and through MAX_GENERATION_SIZE
 CONFIRM_RESULTS         = False  # Run through each file and check that the conjecture holds
 CLI_OUTPUT              = False  # Set to true to see some extra outputs to the CLI
-
-def readFile(filename):
-
-   global DIMENSIONS
-
-   #with open(filename + ".csv") as file:
-   with open("p3.csv") as file:
-
-      if(DIMENSIONS == 2):
-         # Read in partition data, strip for safety
-         partition = list(map(int, file.readline().strip().split(',')))
-         # Ensure that the partition is in standard form for return
-         partition.sort(reverse = True)
-      elif(DIMENSIONS == 3):
-         # Each number on a line is the stack height of the plane partition
-         partition = list()
-         while True:
-            line = file.readline()
-            if not line:
-               break
-            partition.append(list(map(int, line.strip().split(','))))
-      else: return []
-   return partition
 
 
 def d3Hooks(partition):
@@ -66,72 +42,12 @@ def calcHooks(partition):
    # Create the inverse partition
    invPartition = []
 
-   if(DIMENSIONS == 2):
-      if(CLI_OUTPUT): 
-         print("Partition:", *partition)
-
-      # Find the inverse of the partition (swap rows and columns)
-      invPartition = [len(partition) - bisect_right(partition[::-1], i) for i in range(partition[0])]
-      hooks = [[(partition[i] - j + invPartition[j] - i - 1) for j in range(partition[i])] for i in range(len(partition))]
-      
-      # Print hook length results to console
-      print("Hook lengths:")
-      for s in hooks: print(*s)
-   elif(DIMENSIONS == 3):
-      if(CLI_OUTPUT):
-         print("Partition:")
-         for s in partition: print(*s)
+   if(CLI_OUTPUT):
+      print("Partition:")
+      for s in partition: print(*s)
 
       hooks = d3Hooks(partition)
-   else:
-      print("No recognized partition dimension.")
-      return
    return hooks
-
-
-def verifySYT(syt):
-
-   for row in range(len(syt)):
-      for col in range(len(syt[row])):
-         try:
-            # Determine if right square value is larger
-            if syt[row][col] > syt[row][col + 1]:
-               return 0
-         # All exceptions will be an out of bounds error
-         # Ignore them to save time on checks because they are a minimal case
-         except: pass
-
-         try:
-            # Determine if down square value is larger
-            if syt[row][col] > syt[row + 1][col]:
-               return 0
-         # All exceptions will be an out of bounds error
-         # Ignore them to save time on checks because they are a minimal case
-         except: pass
-   return 1
-
-
-def countSYT(partition):
-
-   # Standard is to work with (1, ... , n), add 1 to i to preserve this
-   numbers = [i + 1 for i in range(sum(len(row) for row in partition))]
-
-   # Copy the partition to avoid destructive behavior
-   syt = partition[:]
-   count = 0
-   
-   for perm in permutations(numbers):
-      # Break if the first number is not 1, lowers to (n-1)! options
-      if(perm[0] != 1): break
-
-      # Copy the partition to avoid destructive behavior
-      pIter = iter(perm)
-
-      # Overwrite all values in syt with values from current perm
-      syt = [[next(pIter) for __ in row] for row in syt]
-      count += verifySYT(syt)
-   
-   return count
 
 
 def verifyPSYT(psyt):
@@ -378,35 +294,24 @@ def generateResults():
 
 def main():
 
-   if(DIMENSIONS == 2):
+   # Generate partitions for use in the program.
+   if(GENERATE_3D_PARTITIONS):
+      for i in range(MIN_GENERATION_SIZE, MAX_GENERATION_SIZE + 1):
+         genPartitions(i)
+   
+   if(GENERATE_RESULTS):
+      generateResults()
 
-      partition = readFile(input("Enter the name of the CSV file without the extension: "))
-      hooks = calcHooks(partition)
-      
-      # Output the calculated number of SYT:
-      count = factorial(sum(partition)) / prod([i for sub in hooks for i in sub])
-      print("Count:", int(count))
+   if(CONFIRM_RESULTS):
 
-   if(DIMENSIONS == 3):
-      
-      # Generate partitions for use in the program.
-      if(GENERATE_3D_PARTITIONS):
-         for i in range(MIN_GENERATION_SIZE, MAX_GENERATION_SIZE + 1):
-            genPartitions(i)
-      
-      if(GENERATE_RESULTS):
-         generateResults()
-
-      if(CONFIRM_RESULTS):
-
-         # Loop through all files of size i up to the MAX_GENERATION_SIZE
-         for i in range(1, MAX_GENERATION_SIZE + 1):
-            print(i)
-            with open(f"partition_shapes\size{i}parts.csv", "r") as file:
-               line = file.readline()
-               if(line[0] < line[1]):
-                  print("Conjecture is False")
-                  print(f"{i}: {line}")
+      # Loop through all files of size i up to the MAX_GENERATION_SIZE
+      for i in range(1, MAX_GENERATION_SIZE + 1):
+         print(i)
+         with open(f"partition_shapes\size{i}parts.csv", "r") as file:
+            line = file.readline()
+            if(line[0] < line[1]):
+               print("Conjecture is False")
+               print(f"{i}: {line}")
    
    return
 
